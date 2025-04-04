17081af12ccd14df36cb36a3ba3e9e1c1d1240cc
fmelvin <melvin.favaro@gmail.com>
2025-04-04 09:52:35 +0200

    fix: added Plateau class documentation

--- Game/Plateau.py
+++ Game/Plateau.py
@@ -17,6 +17,9 @@ class Plateau:
         self.winner = 0
         self.shots = []
 
+    """
+    Displays the board in the console as a grid with colored tokens
+    """
     def display_plateau(self):
         player_color = "\033[93m●\033[0m"
         ia_color = "\033[91m●\033[0m"
@@ -38,6 +41,9 @@ class Plateau:
 
         print(" ---------------\n")
 
+    """
+    Displays the player or AI index depending on who is playing, and calls their game function
+    """
     def player_action(self):
         print(f"Player {self.current_player}'s turn!")
 
@@ -47,12 +53,21 @@ class Plateau:
         elif self.current_player == -1:
             IA.ia_choice(self)
 
+    """
+    Toggles the player's hint to play based on the previous player
+    """
     def switch_player(self):
         if self.current_player == 1:
             self.current_player = -1
         else:
             self.current_player = 1
 
+    """
+    Checks the game board for a winning condition.
+    
+    Returns:
+            int: 1 if the human player wins, -1 if the AI wins, 0 if no winner yet.
+    """
     def get_player_to_win(self):
         for line in range(6):
             for column in range(7):
@@ -74,6 +89,9 @@ class Plateau:
                         return self.plateau[line][column]
         return 0
 
+    """
+    Checks if there is a winner or the game is a draw and updates the game state accordingly.
+    """
     def check_win(self):
         if self.get_player_to_win() == 1:
             self.game_over = True
@@ -92,9 +110,15 @@ class Plateau:
             self.display_plateau()
             print("The game is a draw because the board is full!")
 
+    """
+    Calls the save_new_game method of the Database class to save the current game state to a CSV file
+    """
     def save_game(self):
         Database.save_new_game(self.player_who_starts, self.winner, self.shots_played_player, self.shots_played_ia, self.shots)
 
+    """
+    Prompts the user to choose who starts the game between human, AI, or random
+    """
     def player_choice_who_starts(self):
         print("Player who starts!")
         print("1. You")
@@ -121,6 +145,9 @@ class Plateau:
             except ValueError:
                 print("Please enter a number.")
 
+    """
+    Starts the game by initializing the starting player and managing the game loop until the game ends.
+    """
     def start_game(self):
         self.player_choice_who_starts()
 
